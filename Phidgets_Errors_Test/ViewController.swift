08400862d580854c8a2bad09eddb73f27e40f7d1
joelisreal <joel.igberase@gmail.com>
2019-04-18 13:08:53 -0600

    Initial Commit

---
+++ Phidgets_Errors_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phidgets_Errors_Test
+//
+//  Created by Joel Igberase on 2019-04-18.
+//  Copyright © 2019 Joel Igberase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
