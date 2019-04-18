a1817b19c5fab27f89d20a7329742274c056bff5
joelisreal <joel.igberase@gmail.com>
2019-04-18 13:17:43 -0600

    Added pod file, imported fidgets module and wrote function to detect errors

--- Phidgets_Errors_Test/ViewController.swift
+++ Phidgets_Errors_Test/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import Phidget22Swift
 
 class ViewController: UIViewController {
 
+    let led = DigitalOutput()
+    
+    func error_handler(sender: Phidget, data: (code: ErrorEventCode, description: String)) {
+        print("Phidget Error Event: " + data.description)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do{
+            //address object - Error will be generated from this line of code
+            try led.setHubPort(8)
+            
+            let _ = led.error.addHandler(error_handler)
+            
+        } catch let err as PhidgetError{
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
     }
-
-
 }
 
---
+++ Pods/Target Support Files/Pods-Phidgets_Errors_Test/Pods-Phidgets_Errors_Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Phidgets_Errors_Test : NSObject
+@end
+@implementation PodsDummy_Pods_Phidgets_Errors_Test
+@end
